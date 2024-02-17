868e94f1e5e581bf790237790d44495ce2fdaa9b
jerinchy09 <jerintasnimchy09@gmail.com>
2024-02-17 12:21:17 -0500

    first selenium commit

---
+++ src/main/java/selenium_jenkins_maven/selenium_jenkins_maven/App.java
@@ -0,0 +1,13 @@
+package selenium_jenkins_maven.selenium_jenkins_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/selenium_jenkins_maven/selenium_jenkins_maven/AppTest.java
@@ -0,0 +1,21 @@
+package selenium_jenkins_maven.selenium_jenkins_maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void selenium_webdriver()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
